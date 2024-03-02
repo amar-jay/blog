e65f41d954a2d8465075c8abda999682317c212a
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-03-03 02:34:43 +0300

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,134 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+	corePlugins: {
+		// disable aspect ratio as per docs -> @tailwindcss/aspect-ratio
+		aspectRatio: false,
+		borderOpacity: false,
+		fontVariantNumeric: false,
+		ringOffsetColor: false,
+		ringOffsetWidth: false,
+		scrollSnapType: false,
+		textOpacity: false,
+		// disable some core plugins as they are included in the css, even when unused
+		touchAction: false,
+	},
+	darkMode: ["class", '[data-theme="dark"]'],
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/aspect-ratio"),
+		plugin(function ({ addComponents }) {
+			addComponents({
+				".cactus-link": {
+					"&:hover": {
+						backgroundImage:
+							"linear-gradient(transparent,transparent 4px,hsl(var(--theme-link)) 4px,hsl(var(--theme-link)))",
+					},
+					"@apply bg-[size:100%_6px] bg-bottom bg-repeat-x": {},
+					backgroundImage:
+						"linear-gradient(transparent,transparent 5px,hsl(var(--theme-text)) 5px,hsl(var(--theme-text)))",
+				},
+				".title": {
+					"@apply text-2xl font-semibold text-accent-2": {},
+				},
+			});
+		}),
+	],
+	theme: {
+		extend: {
+			colors: {
+				accent: "hsl(var(--theme-accent) / <alpha-value>)",
+				"accent-2": "hsl(var(--theme-accent-2) / <alpha-value>)",
+				bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
+				link: "hsl(var(--theme-link) / <alpha-value>)",
+				quote: "hsl(var(--theme-quote) / <alpha-value>)",
+				textColor: "hsl(var(--theme-text) / <alpha-value>)",
+			},
+			fontFamily: {
+				// Add any custom fonts here
+				sans: [...fontFamily.sans],
+				serif: [...fontFamily.serif],
+			},
+			transitionProperty: {
+				height: "height",
+			},
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			// Remove above once tailwindcss exposes theme type
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						a: {
+							"@apply cactus-link no-underline": "",
+						},
+						blockquote: {
+							borderLeftWidth: "0",
+						},
+						code: {
+							border: "1px dotted #666",
+							borderRadius: "2px",
+						},
+						hr: {
+							borderTopStyle: "dashed",
+						},
+						strong: {
+							fontWeight: "700",
+						},
+						sup: {
+							"@apply ms-0.5": "",
+							a: {
+								"&:after": {
+									content: "']'",
+								},
+								"&:before": {
+									content: "'['",
+								},
+								"&:hover": {
+									"@apply text-link no-underline bg-none": "",
+								},
+								"@apply bg-none": "",
+							},
+						},
+						"tbody tr": {
+							borderBottomWidth: "none",
+						},
+						tfoot: {
+							borderTop: "1px dashed #666",
+						},
+						thead: {
+							borderBottomWidth: "none",
+						},
+						"thead th": {
+							borderBottom: "1px dashed #666",
+							fontWeight: "700",
+						},
+					},
+				},
+				cactus: {
+					css: {
+						"--tw-prose-body": theme("colors.textColor / 1"),
+						"--tw-prose-bold": theme("colors.textColor / 1"),
+						"--tw-prose-bullets": theme("colors.textColor / 1"),
+						"--tw-prose-code": theme("colors.textColor / 1"),
+						"--tw-prose-headings": theme("colors.accent-2 / 1"),
+						"--tw-prose-hr": "0.5px dashed #666",
+						"--tw-prose-links": theme("colors.textColor / 1"),
+						"--tw-prose-quotes": theme("colors.quote / 1"),
+						"--tw-prose-th-borders": "#666",
+					},
+				},
+				sm: {
+					css: {
+						code: {
+							fontSize: theme("fontSize.sm")[0],
+							fontWeight: "400",
+						},
+					},
+				},
+			}),
+		},
+	},
+};
