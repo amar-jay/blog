9a9aaa22527b27c3758a475a0165b041b4d082b6
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-03-03 13:26:54 +0300

    theme and colors

--- astro.config.ts
+++ astro.config.ts
@@ -16,48 +16,61 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  // ! Please remember to replace the following site property with your own domain
-  site: "https://amarjay.vercel.app/",
-  markdown: {
-    remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkMath],
-    rehypePlugins: [rehypeKatex, [rehypeExternalLinks, {
-      target: "_blank",
-      rel: ["nofollow, noopener, noreferrer"]
-    }]],
-    remarkRehype: {
-      footnoteLabelProperties: {
-        className: [""]
-      }
-    }
-  },
-  integrations: [expressiveCode(expressiveCodeOptions), icon(), tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), react()],
-  image: {
-    domains: ["webmention.io", "manans-site.vercel.app"]
-  },
-  // https://docs.astro.build/en/guides/prefetch/
-  prefetch: true,
-  vite: {
-    plugins: [rawFonts([".ttf", ".woff"])],
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+	// ! Please remember to replace the following site property with your own domain
+	site: "https://amarjay.vercel.app/",
+	markdown: {
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkMath],
+		rehypePlugins: [
+			rehypeKatex,
+			[
+				rehypeExternalLinks,
+				{
+					target: "_blank",
+					rel: ["nofollow, noopener, noreferrer"],
+				},
+			],
+		],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: [""],
+			},
+		},
+	},
+	integrations: [
+		expressiveCode(expressiveCodeOptions),
+		icon(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		mdx(),
+		react(),
+	],
+	image: {
+		domains: ["webmention.io", "manans-site.vercel.app", "fonts.googleapis.com"],
+	},
+	// https://docs.astro.build/en/guides/prefetch/
+	prefetch: true,
+	vite: {
+		plugins: [rawFonts([".ttf", ".woff"])],
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
 });
 function rawFonts(ext: Array<string>) {
-  return {
-    name: "vite-plugin-raw-fonts",
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore:next-line
-    transform(_, id) {
-      if (ext.some(e => id.endsWith(e))) {
-        const buffer = fs.readFileSync(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null
-        };
-      }
-    }
-  };
+	return {
+		name: "vite-plugin-raw-fonts",
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore:next-line
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,56 +1,36 @@
+import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 import plugin from "tailwindcss/plugin";
 
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+	darkMode: ["class", '[data-theme="dark"]'],
 	corePlugins: {
 		// disable aspect ratio as per docs -> @tailwindcss/aspect-ratio
 		aspectRatio: false,
-		borderOpacity: false,
-		fontVariantNumeric: false,
-		ringOffsetColor: false,
-		ringOffsetWidth: false,
-		scrollSnapType: false,
-		textOpacity: false,
 		// disable some core plugins as they are included in the css, even when unused
 		touchAction: false,
+		ringOffsetWidth: false,
+		ringOffsetColor: false,
+		scrollSnapType: false,
+		borderOpacity: false,
+		textOpacity: false,
+		fontVariantNumeric: false,
 	},
-	darkMode: ["class", '[data-theme="dark"]'],
-	plugins: [
-		require("@tailwindcss/typography"),
-		require("@tailwindcss/aspect-ratio"),
-		plugin(function ({ addComponents }) {
-			addComponents({
-				".cactus-link": {
-					"&:hover": {
-						backgroundImage:
-							"linear-gradient(transparent,transparent 4px,hsl(var(--theme-link)) 4px,hsl(var(--theme-link)))",
-					},
-					"@apply bg-[size:100%_6px] bg-bottom bg-repeat-x": {},
-					backgroundImage:
-						"linear-gradient(transparent,transparent 5px,hsl(var(--theme-text)) 5px,hsl(var(--theme-text)))",
-				},
-				".title": {
-					"@apply text-2xl font-semibold text-accent-2": {},
-				},
-			});
-		}),
-	],
 	theme: {
 		extend: {
 			colors: {
+				bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
+				textColor: "hsl(var(--theme-text) / <alpha-value>)",
+				link: "hsl(var(--theme-link) / <alpha-value>)",
 				accent: "hsl(var(--theme-accent) / <alpha-value>)",
 				"accent-2": "hsl(var(--theme-accent-2) / <alpha-value>)",
-				bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
-				link: "hsl(var(--theme-link) / <alpha-value>)",
 				quote: "hsl(var(--theme-quote) / <alpha-value>)",
-				textColor: "hsl(var(--theme-text) / <alpha-value>)",
 			},
 			fontFamily: {
 				// Add any custom fonts here
-				sans: [...fontFamily.sans],
-				serif: [...fontFamily.serif],
+				sans: ["Raleway", ...fontFamily.sans],
+				serif: ["Lora", ...fontFamily.serif],
 			},
 			transitionProperty: {
 				height: "height",
@@ -59,38 +39,43 @@ export default {
 			// @ts-ignore
 			// Remove above once tailwindcss exposes theme type
 			typography: (theme) => ({
+				cactus: {
+					css: {
+						"--tw-prose-body": theme("colors.textColor / 1"),
+						"--tw-prose-headings": theme("colors.accent-2 / 1"),
+						"--tw-prose-links": theme("colors.textColor / 1"),
+						"--tw-prose-bold": theme("colors.textColor / 1"),
+						"--tw-prose-bullets": theme("colors.textColor / 1"),
+						"--tw-prose-quotes": theme("colors.quote / 1"),
+						"--tw-prose-code": theme("colors.textColor / 1"),
+						"--tw-prose-hr": "0.5px dashed #666",
+						"--tw-prose-th-borders": "#666",
+					},
+				},
 				DEFAULT: {
 					css: {
 						a: {
 							"@apply cactus-link no-underline": "",
 						},
-						blockquote: {
-							borderLeftWidth: "0",
+						strong: {
+							fontWeight: "700",
 						},
 						code: {
 							border: "1px dotted #666",
 							borderRadius: "2px",
 						},
+						blockquote: {
+							borderLeftWidth: "0",
+						},
 						hr: {
 							borderTopStyle: "dashed",
 						},
-						strong: {
-							fontWeight: "700",
+						thead: {
+							borderBottomWidth: "none",
 						},
-						sup: {
-							"@apply ms-0.5": "",
-							a: {
-								"&:after": {
-									content: "']'",
-								},
-								"&:before": {
-									content: "'['",
-								},
-								"&:hover": {
-									"@apply text-link no-underline bg-none": "",
-								},
-								"@apply bg-none": "",
-							},
+						"thead th": {
+							fontWeight: "700",
+							borderBottom: "1px dashed #666",
 						},
 						"tbody tr": {
 							borderBottomWidth: "none",
@@ -98,26 +83,21 @@ export default {
 						tfoot: {
 							borderTop: "1px dashed #666",
 						},
-						thead: {
-							borderBottomWidth: "none",
+						sup: {
+							"@apply ms-0.5": "",
+							a: {
+								"@apply bg-none": "",
+								"&:hover": {
+									"@apply text-link no-underline bg-none": "",
+								},
+								"&:before": {
+									content: "'['",
+								},
+								"&:after": {
+									content: "']'",
+								},
+							},
 						},
-						"thead th": {
-							borderBottom: "1px dashed #666",
-							fontWeight: "700",
-						},
-					},
-				},
-				cactus: {
-					css: {
-						"--tw-prose-body": theme("colors.textColor / 1"),
-						"--tw-prose-bold": theme("colors.textColor / 1"),
-						"--tw-prose-bullets": theme("colors.textColor / 1"),
-						"--tw-prose-code": theme("colors.textColor / 1"),
-						"--tw-prose-headings": theme("colors.accent-2 / 1"),
-						"--tw-prose-hr": "0.5px dashed #666",
-						"--tw-prose-links": theme("colors.textColor / 1"),
-						"--tw-prose-quotes": theme("colors.quote / 1"),
-						"--tw-prose-th-borders": "#666",
 					},
 				},
 				sm: {
@@ -131,4 +111,24 @@ export default {
 			}),
 		},
 	},
-};
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/aspect-ratio"),
+		plugin(function ({ addComponents }) {
+			addComponents({
+				".cactus-link": {
+					"@apply bg-[size:100%_6px] bg-bottom bg-repeat-x": {},
+					backgroundImage:
+						"linear-gradient(transparent,transparent 5px,hsl(var(--theme-text)) 5px,hsl(var(--theme-text)))",
+					"&:hover": {
+						backgroundImage:
+							"linear-gradient(transparent,transparent 4px,hsl(var(--theme-link)) 4px,hsl(var(--theme-link)))",
+					},
+				},
+				".title": {
+					"@apply text-2xl font-serif font-semibold text-accent-2": {},
+				},
+			});
+		}),
+	],
+} satisfies Config;
