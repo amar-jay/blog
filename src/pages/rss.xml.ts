8e5e50a105cc8f1bb311664fe5e50bf145fd4e30
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-03-25 19:47:52 +0300

    stuff added

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,14 +4,6 @@ import { getAllPosts } from "@/utils";
 
 const SITE = import.meta.env.SITE;
 
-// set up some custom XML tags to inject into the RSS feed
-//const customDataTags = [
-  // enable Atom feed, as some RSS readers use that format
-  // https://www.fpds.gov/wiki/index.php/FAADC_Atom_Feed_Specifications_V_1.0
-  // enable language metadata
-//  `<language>en-us</language>`,
-//];
-
 export const GET = async () => {
 	const posts = await getAllPosts();
 
@@ -26,6 +18,5 @@ export const GET = async () => {
 			link: `posts/${post.slug}`,
 		})),
 
-//    customData: `<atom:link href="https://amar-jay.vercel.app/rss.xml" rel="self" type="application/rss+xml" />`,
   });
 };
