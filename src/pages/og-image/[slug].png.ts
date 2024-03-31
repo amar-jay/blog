bba40e5ef25ee5a1de7423ab7d823e92d8474428
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2024-03-31 16:30:25 +0300

    og image

--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -29,31 +29,31 @@ const ogOptions: SatoriOptions = {
 	],
 };
 
-const markup = (title: string, pubDate: string) =>
+const markup = (title: string, description: string, pubDate: string) =>
 	html`<div tw="flex flex-col w-full h-full bg-[#1d1f21] text-[#c9cacc]">
-		<div tw="flex flex-col flex-1 w-full p-10 justify-center">
+		<div tw="flex flex-col flex-1 w-full p-10 justify-center flex-wrap">
 			<p tw="text-2xl mb-6">${pubDate}</p>
 			<h1 tw="text-6xl font-bold leading-snug text-white">${title}</h1>
+			<p tw="text-2xl leading-snug text-wrap wrap">${description}</p>
 		</div>
-		<div tw="flex items-center justify-between w-full p-10 border-t border-[#2bbc89] text-xl">
+		<div tw="flex items-center justify-between w-full p-5 border-t border-[#2bbc89] text-xl">
 			<div tw="flex items-center">
 				<p tw="ml-3 font-semibold">${siteConfig.title}</p>
 			</div>
 			<h4>by ${siteConfig.author}</h4>
-			<p>${siteConfig.description}</p>
 		</div>
 	</div>`;
 
 type Props = InferGetStaticPropsType<typeof getStaticPaths>;
 
 export async function GET(context: APIContext) {
-	const { title, pubDate } = context.props as Props;
+	const { title, pubDate, description } = context.props as Props;
 
 	const postDate = getFormattedDate(pubDate, {
 		weekday: "long",
 		month: "long",
 	});
-	const svg = await satori(markup(title, postDate) as unknown as ReactNode, ogOptions);
+	const svg = await satori(markup(title, description, postDate) as unknown as ReactNode, ogOptions);
 	const png = new Resvg(svg).render().asPng();
 	return new Response(png, {
 		headers: {
@@ -71,6 +71,7 @@ export async function getStaticPaths() {
 			params: { slug: post.slug },
 			props: {
 				title: post.data.title,
+				description: post.data.description,
 				pubDate: post.data.updatedDate ?? post.data.publishDate,
 			},
 		}));
--- src/site-config.ts
+++ src/site-config.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Amar Jay",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "The Blog",
+	title: "Underscore",
 	// Meta property used as the default description meta property
 	description: "Opinionated thought, ideas and experiences of Amar Jay",
 	// HTML lang property, found in src/layouts/Base.astro L:18
