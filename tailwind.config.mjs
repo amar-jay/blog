2c4702bb111c0ad9f86059831c2de25fa45abcc1
amar-jay <64834413+amar-jay@users.noreply.github.com>
2024-05-24 16:30:01 +0300

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -157,7 +157,7 @@ export default {
 						"--tw-prose-body": theme("colors.textColor / 1"),
 						"--tw-prose-bold": theme("colors.textColor / 1"),
 						"--tw-prose-bullets": theme("colors.textColor / 1"),
-						"--tw-prose-code": theme("colors.textColor / 1"),
+						"--tw-prose-code": theme("colors.quote / 1"),
 						"--tw-prose-headings": theme("colors.accent-2 / 1"),
 						"--tw-prose-hr": "0.5px dashed #666",
 						"--tw-prose-links": theme("colors.textColor / 1"),
