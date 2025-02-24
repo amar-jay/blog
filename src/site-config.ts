f6cf4556708f1a05878c51c87a6a66baa6683cf5
amar-jay <64834413+amar-jay@users.noreply.github.com>
2025-02-24 23:23:05 +0300

    navbar change

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,31 +1,41 @@
 import type { SiteConfig } from "@/types";
 import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
-export const siteConfig: SiteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Amar Jay",
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "amar-jay",
-	// Meta property used as the default description meta property
-	description: "🍴😴🪳🍹",
-	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
-	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
-	date: {
-		locale: "en-GB",
-		options: {
-			day: "numeric",
-			month: "short",
-			year: "numeric",
+export const siteConfig: SiteConfig =
+	{
+		// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+		author:
+			"Amar Jay",
+		// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+		title:
+			"amar-jay",
+		// Meta property used as the default description meta property
+		description:
+			"One Love ☝️️",
+		// HTML lang property, found in src/layouts/Base.astro L:18
+		lang: "en-GB",
+		// Meta property, found in src/components/BaseHead.astro L:42
+		ogLocale:
+			"en_GB",
+		// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+		date: {
+			locale:
+				"en-GB",
+			options:
+				{
+					day: "numeric",
+					month:
+						"short",
+					year: "numeric",
+				},
 		},
-	},
-	webmentions: {
-		link: "https://webmention.io/amarjay.vercel.app/webmention",
-    pingback: "https://webmention.io/amarjay.vercel.app/xmlrpc",
-	},
-};
+		webmentions:
+			{
+				link: "https://webmention.io/amarjay.vercel.app/webmention",
+				pingback:
+					"https://webmention.io/amarjay.vercel.app/xmlrpc",
+			},
+	};
 
 // Used to generate links in both the Header & Footer.
 export const menuLinks: {
@@ -33,49 +43,85 @@ export const menuLinks: {
 	path: string;
 }[] = [
 	{
-		title: "Home",
+		title:
+			"Home",
 		path: "/",
 	},
 	{
-		title: "Blog",
+		title:
+			"Blog",
 		path: "/posts/",
 	},
-    {
-		title: "Projects",
-		path: "/projects/",
-	},
 	{
-		title: "Tags",
-		path: "/tags/",
+		title:
+			"Projects",
+		path: "/projects/",
 	},
 ];
 
 // https://expressive-code.com/reference/configuration/
-export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
-	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["dracula", "github-light"],
-	themeCssSelector(theme, { styleVariants }) {
-		// If one dark and one light theme are available
-		// generate theme CSS selectors compatible with cactus-theme dark mode switch
-		if (styleVariants.length >= 2) {
-			const baseTheme = styleVariants[0]?.theme;
-			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme;
-			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`;
-		}
-		// return default selector
-		return `[data-theme="${theme.name}"]`;
-	},
-	useThemedScrollbars: false,
-	styleOverrides: {
-		frames: {
-			frameBoxShadowCssValue: "none",
+export const expressiveCodeOptions: AstroExpressiveCodeOptions =
+	{
+		// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
+		themes:
+			[
+				"dracula",
+				"github-light",
+			],
+		themeCssSelector(
+			theme,
+			{
+				styleVariants,
+			}
+		) {
+			// If one dark and one light theme are available
+			// generate theme CSS selectors compatible with cactus-theme dark mode switch
+			if (
+				styleVariants.length >=
+				2
+			) {
+				const baseTheme =
+					styleVariants[0]
+						?.theme;
+				const altTheme =
+					styleVariants.find(
+						v =>
+							v
+								.theme
+								.type !==
+							baseTheme?.type
+					)?.theme;
+				if (
+					theme ===
+						baseTheme ||
+					theme ===
+						altTheme
+				)
+					return `[data-theme='${theme.type}']`;
+			}
+			// return default selector
+			return `[data-theme="${theme.name}"]`;
 		},
-		uiLineHeight: "inherit",
-		codeFontSize: "0.875rem",
-		codeLineHeight: "1.7142857rem",
-		borderRadius: "4px",
-		codePaddingInline: "1rem",
-		codeFontFamily:
-			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
-	},
-};
+		useThemedScrollbars:
+			false,
+		styleOverrides:
+			{
+				frames:
+					{
+						frameBoxShadowCssValue:
+							"none",
+					},
+				uiLineHeight:
+					"inherit",
+				codeFontSize:
+					"0.875rem",
+				codeLineHeight:
+					"1.7142857rem",
+				borderRadius:
+					"4px",
+				codePaddingInline:
+					"1rem",
+				codeFontFamily:
+					'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
+			},
+	};
